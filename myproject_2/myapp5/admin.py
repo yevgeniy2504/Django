b8d2099a3ffcb901663961eaeca73d0f3ec56177
Yevgeniy Karabekov <yevgeniy.karabekov@gmail.com>
2023-10-29 22:09:02 +0600

    lesson_5

--- myproject_2/myapp5/admin.py
+++ myproject_2/myapp5/admin.py
@@ -5,6 +5,11 @@ from .models import Category, Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'image', 'is_delete']
     list_filter = ['price', 'name']
+    fields = [
+        ('name', 'category'),
+        ('price', 'stock'),
+        ('discount', 'image'),
+    ]
 
 
 class CategoryAdmin(admin.ModelAdmin):
